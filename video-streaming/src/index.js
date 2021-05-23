0f5e691734a329dec7729c06bfb609e913b50367
Brian Khunchana <briankhunchana@gmail.com>
2021-05-23 14:32:58 -0500

    Added recommendation service and rabbitmq for comm

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,58 +1,103 @@
 const express = require('express')
-const http = require('http')
-const mongodb = require('mongodb')
-const app = express()
-const PORT = process.env.PORT
-const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST
-const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT)
-const DBHOST = process.env.DBHOST
-const DBNAME = process.env.DBNAME
-function main() {
-  return mongodb.MongoClient.connect(DBHOST).then((client) => {
-    const db = client.db(DBNAME)
-    const videosCollection = db.collection('videos')
+const fs = require('fs')
+const amqp = require('amqplib')
 
-    app.get('/video', (req, res) => {
-      const videoId = new mongodb.ObjectID(req.query.id)
-      videosCollection
-        .findOne({ _id: videoId })
-        .then((videoRecord) => {
-          if (!videoRecord) {
-            res.sendStatus(404)
-            return
-          }
+if (!process.env.RABBIT) {
+  throw new Error(
+    'Please specify the name of the RabbitMQ host using environment variable RABBIT'
+  )
+}
 
-          const forwardRequest = http.request(
-            {
-              host: VIDEO_STORAGE_HOST,
-              port: VIDEO_STORAGE_PORT,
-              path: `/video?path=${videoRecord.videoPath}`,
-              method: 'GET',
-              headers: req.headers,
-            },
-            (forwardResponse) => {
-              res.writeHeader(
-                forwardResponse.statusCode,
-                forwardResponse.headers
-              )
-              forwardResponse.pipe(res)
-            }
-          )
+const RABBIT = process.env.RABBIT
 
-          req.pipe(forwardRequest)
-        })
-        .catch((err) => {
-          console.error('Database query failed.')
-          console.error((err && err.stack) || err)
-          res.sendStatus(500)
+//
+// Connect to the RabbitMQ server.
+//
+function connectRabbit() {
+  console.log(`Connecting to RabbitMQ server at ${RABBIT}.`)
+
+  return amqp
+    .connect(RABBIT) // Connect to the RabbitMQ server.
+    .then((connection) => {
+      console.log('Connected to RabbitMQ.')
+
+      return connection
+        .createChannel() // Create a RabbitMQ messaging channel.
+        .then((messageChannel) => {
+          return messageChannel
+            .assertExchange('viewed', 'fanout') // Assert that we have a "viewed" exchange.
+            .then(() => {
+              return messageChannel
+            })
         })
     })
+}
 
-    app.listen(PORT, () => {
-      console.log(`Microservice online.`)
+//
+// Send the "viewed" to the history microservice.
+//
+function sendViewedMessage(messageChannel, videoPath) {
+  console.log(`Publishing message on "viewed" exchange.`)
+
+  const msg = { videoPath: videoPath }
+  const jsonMsg = JSON.stringify(msg)
+  messageChannel.publish('viewed', '', Buffer.from(jsonMsg)) // Publish message to the "viewed" exchange.
+}
+
+//
+// Setup event handlers.
+//
+function setupHandlers(app, messageChannel) {
+  app.get('/video', (req, res) => {
+    // Route for streaming video.
+
+    const videoPath = './videos/SampleVideo_1280x720_1mb.mp4'
+    fs.stat(videoPath, (err, stats) => {
+      if (err) {
+        console.error('An error occurred ')
+        res.sendStatus(500)
+        return
+      }
+
+      res.writeHead(200, {
+        'Content-Length': stats.size,
+        'Content-Type': 'video/mp4',
+      })
+
+      fs.createReadStream(videoPath).pipe(res)
+
+      sendViewedMessage(messageChannel, videoPath) // Send message to "history" microservice that this video has been "viewed".
     })
   })
 }
+
+//
+// Start the HTTP server.
+//
+function startHttpServer(messageChannel) {
+  return new Promise((resolve) => {
+    // Wrap in a promise so we can be notified when the server has started.
+    const app = express()
+    setupHandlers(app, messageChannel)
+
+    const port = (process.env.PORT && parseInt(process.env.PORT)) || 3000
+    app.listen(port, () => {
+      resolve() // HTTP server is listening, resolve the promise.
+    })
+  })
+}
+
+//
+// Application entry point.
+//
+function main() {
+  return connectRabbit() // Connect to RabbitMQ...
+    .then((messageChannel) => {
+      // then...
+      return startHttpServer(messageChannel) // start the HTTP server.
+    })
+}
+
 main()
   .then(() => console.log('Microservice online.'))
   .catch((err) => {
