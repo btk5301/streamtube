07551d5c2a43d63a05e90b0d60e47a634e3b9336
Brian Khunchana <briankhunchana@gmail.com>
2021-05-24 23:54:04 -0500

    change backend tf file

--- scripts/backend.tf
+++ scripts/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "azurerm" {                       
         resource_group_name  = "terrastore"    
         storage_account_name = "testore"    
-        container_name       = "terraform"    
+        container_name       = "terrastore"    
         key = "terraform.tfstate"             
     }                                         
 }
